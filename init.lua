e57d61cd20b59ef0612c19d454742f7bfb8e806a
Jukka Aho <juaho@student.hive.fi>
2024-12-16 13:30:01 +0200

    nvim conf

---
+++ init.lua
@@ -0,0 +1,26 @@
+require("config.lazy")
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("lspconfig").clangd.setup {}
+require("tokyonight").setup({
+	style = "moon",
+	styles = {
+		keywords = { italic = false },
+	},
+	-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+	on_colors = function(colors)
+	colors.hint = colors.orange
+	colors.error = "#ff0000"
+	end
+})
+--------------------------------------------------------------------------------
+vim.cmd[[colorscheme tokyonight-moon]]
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.wo.number = true
+vim.opt.colorcolumn = "81"
+vim.cmd("highlight ColorColumn ctermbg=0 guibg=#ff0000")
+vim.keymap.set('n', '<F7>', '<CMD> tabp <CR>', {silent = true})
+vim.keymap.set('n', '<F8>', '<CMD> tabn <CR>', {silent = true})
+vim.keymap.set('i', '<F7>', '<ESC> <CMD> tabp <CR>', {silent = true})
+vim.keymap.set('i', '<F8>', '<ESC> <CMD> tabn <CR>', {silent = true})
---
+++ lua/plugins/spec1.lua
@@ -0,0 +1,12 @@
+return
+{
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	}
+}
