7af9051a3c63646f4e48c7892d5e728c2d57610d
Jukka Aho <juaho@c1r5p12.hive.fi>
2024-12-17 12:42:58 +0200

    added 42header

--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -8,5 +8,20 @@ return
 		lazy = false,
 		priority = 1000,
 		opts = {},
+	},
+	{
+		"Diogo-ss/42-header.nvim",
+		cmd = { "Stdheader" },
+		keys = { "<F1>" },
+		opts = {
+			default_map = true, -- Default mapping <F1> in normal mode.
+			auto_update = true, -- Update header when saving.
+			user = "juaho", -- Your user.
+			mail = "juaho@student.hive.fi", -- Your mail.
+    -- add other options.
+		},
+		config = function(_, opts)
+		require("42header").setup(opts)
+		end,
 	}
 }
