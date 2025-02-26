f19785eb7c00797ad9ecf3be5aef3ac52cd1baaf
Jukka Aho <juaho@student.hive.fi>
2025-02-26 19:32:06 +0200

    Update

--- init.lua
+++ init.lua
@@ -17,15 +17,4 @@ require("tokyonight").setup({
 require("nvim-function-linecount")
 --------------------------------------------------------------------------------
 vim.cmd[[colorscheme tokyonight-moon]]
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
-vim.wo.number = true
-vim.opt.colorcolumn = "81"
 vim.cmd("highlight ColorColumn ctermbg=0 guibg=#ff0000")
-vim.keymap.set('n', '<Up>', '<C-w>k', {silent = true})
-vim.keymap.set('n', '<Down>', '<C-w>j', {silent = true})
-vim.keymap.set('n', '<Left>', '<C-w>h', {silent = true})
-vim.keymap.set('n', '<Right>', '<C-w>l', {silent = true})
-vim.keymap.set('n', '<C-Left>', '<cmd>:tabprev<CR>', {silent = true})
-vim.keymap.set('n', '<C-Right>', '<cmd>:tabnext<CR>', {silent = true})
-vim.keymap.set('n', '``', '<cmd>:Neotree toggle=true reveal=true<CR>', {silent = true})
