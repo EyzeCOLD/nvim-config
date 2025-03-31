b4f62ec1c6dba10a5483d86eb510ed20f354b799
Jukka Aho <juaho@student.hive.fi>
2025-03-31 11:13:19 +0300

    telescope

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,7 @@ vim.o.tabstop = 4
 vim.o.relativenumber = true
 vim.o.number = true
 vim.o.colorcolumn = "81"
+vim.diagnostic.config({ virtual_text = true })
 vim.keymap.set('n', '<Up>', '<C-w>k', {silent = true})
 vim.keymap.set('n', '<Down>', '<C-w>j', {silent = true})
 vim.keymap.set('n', '<Left>', '<C-w>h', {silent = true})
@@ -47,3 +48,12 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, { desc = 'Goto definition' })
+vim.keymap.set('n', '<leader>ft', builtin.lsp_type_definitions, { desc = 'Goto type definition' })
+vim.keymap.set('n', '<leader>fi', builtin.lsp_implementations, { desc = 'Goto implementation' })
--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -36,5 +36,15 @@ return
 	},
 	{
 		"EyzeCOLD/nvim-function-linecount",
-	}
+	},
+	{
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+	{
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+},
 }
