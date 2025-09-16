fc0c74c5f44b4b64f30a579c9c0048a486a1a67a
juaho <juaho@student.hive.fi>
2025-09-16 12:42:09 +0300

    nvim cmp and luasnip added

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 lazy.nvim
-- mason-lspconfig.nvim
-- mason.nvim
-- neo-tree.nvim
-- nui.nvim
-- nvim-function-linecount
-- nvim-lspconfig
-- nvim-web-devicons
-- plenary.nvim
-- tokyonight.nvim
-- 42-header.nvim
+    ● cmp-buffer  nvim-cmp
+    ● cmp-cmdline  nvim-cmp
+    ● cmp-nvim-lsp  nvim-cmp
+    ● cmp-path  nvim-cmp
+    ● cmp_luasnip  nvim-cmp
+    ● lazy.nvim  init.lua
+    ● LuaSnip  nvim-cmp
+    ● mason-lspconfig.nvim  start
+    ● mason.nvim  start
+    ● neo-tree.nvim  start
+    ● nui.nvim  neo-tree.nvim
+    ● nvim-cmp  start
+    ● nvim-function-linecount  start
+    ● nvim-lspconfig  start
+    ● nvim-treesitter  start
+    ● nvim-web-devicons  neo-tree.nvim
+    ● plenary.nvim  telescope.nvim
+    ● telescope.nvim  start
+    ● tokyonight.nvim  start
+    ○ 42-header.nvim <F1>
---
+++ lua/plugins/nvim-cmp-luasnip.lua
@@ -0,0 +1,65 @@
+return
+{
+	{
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+
+			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-e>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.abort()
+						elseif luasnip.jumpable(1) or luasnip.jumpable(-1) then
+							luasnip.unlink_current()
+						else
+							fallback()
+						end
+					end, { "i", "s", "c" }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
+}
